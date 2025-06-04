e4b827cb3ebd9836003c03e23cc422a93ec24b54
lovit <soy.lovit@gmail.com>
2025-06-05 00:41:51 +0900

    [NoIssue] base url 을 'wine-gallery' 로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/photo-gallery/', // GitHub Pages 배포를 위한 base URL
+  base: '/natural-wine/', // GitHub Pages 배포를 위한 base URL
 })
