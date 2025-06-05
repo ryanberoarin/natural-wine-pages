37737e5970c245b95cdfb7c50c11ac9033969af3
lovit <soy.lovit@gmail.com>
2025-06-05 20:32:27 +0900

    [NoIssue] Description 관리
    변경의 성격을 refactor로 명시 (코드 구조 개선)
    주요 변경사항을 제목에 간단히 요약
    세부 변경사항을 bullet point로 명확하게 설명

---
+++ src/data/gallery25_04.ts
@@ -0,0 +1,82 @@
+import type { ImageData } from '../types'
+
+export const gallery25_04: ImageData[] = [
+  {
+    id: 1,
+    url: 'images/25-04/250401.png',
+    title: 'April 2025 - Image 1',
+    description: 'First image from April 2025'
+  },
+  {
+    id: 2,
+    url: 'images/25-04/250402.png',
+    title: 'April 2025 - Image 2',
+    description: 'Second image from April 2025'
+  },
+  {
+    id: 3,
+    url: 'images/25-04/250403.png',
+    title: 'April 2025 - Image 3',
+    description: 'Third image from April 2025'
+  },
+  {
+    id: 4,
+    url: 'images/25-04/250404.png',
+    title: 'April 2025 - Image 4',
+    description: 'Fourth image from April 2025'
+  },
+  {
+    id: 5,
+    url: 'images/25-04/250405.png',
+    title: 'April 2025 - Image 5',
+    description: 'Fifth image from April 2025'
+  },
+  {
+    id: 6,
+    url: 'images/25-04/250406.png',
+    title: 'April 2025 - Image 6',
+    description: 'Sixth image from April 2025'
+  },
+  {
+    id: 7,
+    url: 'images/25-04/250407.png',
+    title: 'April 2025 - Image 7',
+    description: 'Seventh image from April 2025'
+  },
+  {
+    id: 8,
+    url: 'images/25-04/250408.png',
+    title: 'April 2025 - Image 8',
+    description: 'Eighth image from April 2025'
+  },
+  {
+    id: 9,
+    url: 'images/25-04/250409.png',
+    title: 'April 2025 - Image 9',
+    description: 'Ninth image from April 2025'
+  },
+  {
+    id: 10,
+    url: 'images/25-04/250410.png',
+    title: 'April 2025 - Image 10',
+    description: 'Tenth image from April 2025'
+  },
+  {
+    id: 11,
+    url: 'images/25-04/250411.png',
+    title: 'April 2025 - Image 11',
+    description: 'Eleventh image from April 2025'
+  },
+  {
+    id: 12,
+    url: 'images/25-04/250412.png',
+    title: 'April 2025 - Image 12',
+    description: 'Twelfth image from April 2025'
+  },
+  {
+    id: 13,
+    url: 'images/25-04/250413.png',
+    title: 'April 2025 - Image 13',
+    description: 'Thirteenth image from April 2025'
+  }
+] 
---
+++ src/data/gallery25_05.ts
@@ -0,0 +1,46 @@
+import type { ImageData } from '../types'
+
+export const gallery25_05: ImageData[] = [
+  {
+    id: 1,
+    url: 'images/25-05/250501.png',
+    title: 'LE P\'TIT BLANC',
+    description: 'LE P\'TIT BLANC / France / Loire / Touraine'
+  },
+  {
+    id: 2,
+    url: 'images/25-05/250502.png',
+    title: 'Goutte d\'O',
+    description: 'Goutte d\'O / France / Loire / Touraine'
+  },
+  {
+    id: 3,
+    url: 'images/25-05/250503.png',
+    title: 'Chenin Faisant',
+    description: 'Chenin Faisant / France / Loire / Anjou'
+  },
+  {
+    id: 4,
+    url: 'images/25-05/250504.png',
+    title: 'Les Aussigouins',
+    description: 'Les Aussigouins (2021) / France / Loire / Anjou'
+  },
+  {
+    id: 5,
+    url: 'images/25-05/250505.png',
+    title: 'La Guimardiere',
+    description: 'La Guimardiere / France / Loire / Saumur'
+  },
+  {
+    id: 6,
+    url: 'images/25-05/250506.png',
+    title: 'La Nouette',
+    description: 'La Nouette / France / Loire / Saumur'
+  },
+  {
+    id: 7,
+    url: 'images/25-05/250507.png',
+    title: 'Les Rosiers',
+    description: 'Les Rosiers (2022) / France / Loire / Jasnières'
+  }
+] 
